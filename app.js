670bb935b2ef711bf4b2756398e6664ecde8b9ce
jonathantang <jonathantang0126@gmail.com>
2019-08-25 17:29:39 +0800

    modify search function and add unmatch page

--- app.js
+++ app.js
@@ -28,9 +28,14 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const matchRestaurant = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
   })
-  res.render('index', { restaurants: matchRestaurant, keyword: keyword })
+  if (matchRestaurant.length === 0) {
+    res.render('unmatch', { keyword: keyword })
+  } else {
+    res.render('index', { restaurants: matchRestaurant, keyword: keyword })
+  }
+
 })
 
 // start and listen on the Express server
