4186f9d39e62257a010a91ba0b2496907277546e
jonathantang <jonathantang0126@gmail.com>
2019-09-07 09:56:13 +0800

    Add Mongoose connection and create Todo model

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./Restaurant_List.json')
 
+// 載入 mongoose
+const mongoose = require('mongoose')
+
+// 設定連線到 mongoDB的restaurant
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
+// mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
+const db = mongoose.connection
+
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -15,28 +23,45 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 // routes setting
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+//載入restaurant model
+const Restaurant = require('./models/resaurant')
+
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  res.send('hello world!')
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const focusRestaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+// app.get('/', (req, res) => {
+//   res.render('index', { restaurants: restaurantList.results })
+// })
 
-  res.render('show', { restaurants: focusRestaurant })
-})
+// app.get('/restaurants/:restaurant_id', (req, res) => {
+//   const focusRestaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
 
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const matchRestaurant = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
-  })
-  if (matchRestaurant.length === 0) {
-    res.render('unmatch', { keyword: keyword })
-  } else {
-    res.render('index', { restaurants: matchRestaurant, keyword: keyword })
-  }
+//   res.render('show', { restaurants: focusRestaurant })
+// })
 
-})
+// app.get('/search', (req, res) => {
+//   const keyword = req.query.keyword
+//   const matchRestaurant = restaurantList.results.filter(restaurant => {
+//     return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
+//   })
+//   if (matchRestaurant.length === 0) {
+//     res.render('unmatch', { keyword: keyword })
+//   } else {
+//     res.render('index', { restaurants: matchRestaurant, keyword: keyword })
+//   }
+
+// })
 
 // start and listen on the Express server
 app.listen(port, () => {
---
+++ models/resaurant.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+  name: {
+    type: String,               // 資料型別是字串
+    required: true          // 這是個必填欄位
+  },
+  name_en: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  google_map: {
+    type: String
+  },
+  rating: {
+    type: String
+  },
+  description: {
+    type: String,
+    required: true
+  },
+})
+
+
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
