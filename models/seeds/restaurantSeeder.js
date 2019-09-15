fe322b8bc7e1b2fe1b7c8db1b3b98830c6386f09
jonathantang <jonathantang0126@gmail.com>
2019-09-15 20:33:02 +0800

    modify seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
-const Restaurant = require('../restaurant')
-const restaurantSeedList = require('../../Restaurant_list.json')
+const bcrypt = require('bcryptjs')
 
-mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
+const Restaurant = require('../restaurant')
+const User = require('../user')
+
+const restaurantSeedList = require('./Restaurant_list.json')
+const userSeedList = require('./User_list.json')
+
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useCreateIndex: true })
 
 const db = mongoose.connection
 
@@ -12,11 +17,42 @@ db.on('error', () => {
 
 db.once('open', () => {
   console.log('db connected!')
+  for (let i = 0; i < userSeedList.results.length; i++) {
+    const newUser = new User({
+      name: userSeedList.results[i].name,
+      email: userSeedList.results[i].email,
+      password: userSeedList.results[i].password
+    })
 
-  for (let i = 0; i < restaurantSeedList.results.length; i++) {
-    const item = restaurantSeedList.results[i];
-    Restaurant.create(item)
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err
+        newUser.password = hash
+
+        newUser.save().then(users => {
+          console.log(users)
+          for (var j = i * 3; j < (i * 3) + 3; j++) {
+            Restaurant.create({
+              userId: users._id,
+              name: restaurantSeedList.results[j].name,
+              name_en: restaurantSeedList.results[j].name_en,
+              category: restaurantSeedList.results[j].category,
+              image: restaurantSeedList.results[j].image,
+              location: restaurantSeedList.results[j].location,
+              phone: restaurantSeedList.results[j].phone,
+              google_map: restaurantSeedList.results[j].google_map,
+              rating: restaurantSeedList.results[j].rating,
+              description: restaurantSeedList.results[j].description
+            })
+
+          }
+        }).catch(err => {
+          console.log(err)
+        })
+      })
+    })
+
   }
-
-  console.log('done')
+  console.log('Created seed!')
 })
+
